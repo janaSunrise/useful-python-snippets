7880d4cc14c31032aac91cc8662829800b32f32d
janaSunrise <warriordefenderz@gmail.com>
2021-03-19 16:46:18 +0530

    Add exponential backoff algo.

---
+++ src/algorithm/backoff.py
@@ -0,0 +1,29 @@
+import random
+import time
+import typing as t
+
+
+class ExponentialBackoff:
+    def __init__(self, base: int = 1, *, integral: bool = False) -> None:
+        self._base = base
+
+        self._exp = 0
+        self._max = 10
+        self._reset_time = base * 2 ** 11
+        self._last_invocation = time.monotonic()
+
+        rand = random.Random()
+        rand.seed()
+
+        self._randfunc = rand.randrange if integral else rand.uniform
+
+    def delay(self) -> t.Any:
+        invocation = time.monotonic()
+        interval = invocation - self._last_invocation
+        self._last_invocation = invocation
+
+        if interval > self._reset_time:
+            self._exp = 0
+
+        self._exp = min(self._exp + 1, self._max)
+        return self._randfunc(0, self._base * 2 ** self._exp)
